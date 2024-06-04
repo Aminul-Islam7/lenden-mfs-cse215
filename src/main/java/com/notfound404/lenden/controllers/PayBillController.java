0d5cd77684d33c9707268d53409af5a5e4eb99c6
Naylah953 <naylah100@gmail.com>
2024-06-05 01:14:43 +0600

    feat: Add 'Book Pay Bill' scene

--- src/main/java/com/notfound404/lenden/controllers/PayBillController.java
+++ src/main/java/com/notfound404/lenden/controllers/PayBillController.java
@@ -2,20 +2,20 @@ package com.notfound404.lenden.controllers;
 
 public class PayBillController {
 
-  public void visitPayBillElectricity() {
-    SceneController.setScene("PayBillElectricity.fxml", "Pay Bill - Electricity");
-  }
+    public void visitPayBillElectricity() {
+        SceneController.setScene("PayBillElectricity.fxml", "Pay Bill - Electricity");
+    }
 
-  public void visitPayBillGas() {
-    SceneController.setScene("PayBillGas.fxml", "Pay Bill - Gas");
-  }
+    public void visitPayBillGas() {
+        SceneController.setScene("PayBillGas.fxml", "Pay Bill - Gas");
+    }
 
-  public void visitPayBillWater() {
-    SceneController.setScene("PayBillWater.fxml", "Pay Bill - Water");
-  }
+    public void visitPayBillWater() {
+        SceneController.setScene("PayBillWater.fxml", "Pay Bill - Water");
+    }
 
-  public void visitPayBillInternet() {
-    SceneController.setScene("PayBillInternet.fxml", "Pay Bill - Internet");
-  }
+    public void visitPayBillInternet() {
+        SceneController.setScene("PayBillInternet.fxml", "Pay Bill - Internet");
+    }
 
 }
