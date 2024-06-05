c19a1058d31db93073fad3bcae0e25f1d4450586
ShahriarRakibRabbi <beingshahriarrakib@gmail.com>
2024-06-06 01:14:19 +0600

    feat: Add scenes and controller for PayBill

---
+++ src/main/java/com/notfound404/lenden/controllers/PayBillInternetController.java
@@ -0,0 +1,20 @@
+package com.notfound404.lenden.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class PayBillInternetController {
+
+    @FXML
+    private TextField nameField, idField, amountField, pinField;
+
+    @FXML
+    private Label errorLabel;
+
+    @FXML
+    private void handlePayBill() {
+
+    }
+
+}
