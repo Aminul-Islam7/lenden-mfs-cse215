dfdf3dd4097dbb53a06ec9886ddf41ee40f16000
Naylah953 <naylah100@gmail.com>
2024-06-08 21:06:53 +0600

    feat: AM From Bank

--- src/main/java/com/notfound404/lenden/controllers/AMFromCardController.java
+++ src/main/java/com/notfound404/lenden/controllers/AMFromCardController.java
@@ -1,8 +1,88 @@
 package com.notfound404.lenden.controllers;
 
+import com.notfound404.lenden.models.TransactionInfo;
+import com.notfound404.lenden.models.TransactionType;
+import com.notfound404.lenden.services.TransactionService;
+import com.notfound404.lenden.services.UserService;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class AMFromCardController {
 
-  public void visitAMFromCardSuccess() {
+  @FXML
+  private Label errorLabel;
+
+  @FXML
+  private TextField nameField, numberField, monthField, yrField, securityCodeField, postalCodeField, amountField;
+  @FXML
+  private PasswordField pinField;
+  
+  public static SuccessController successController;
+
+  public static void setSuccessController(SuccessController controller) {
+    successController = controller;
+  }
+
+  @FXML
+  private void handleAddMoney() {
+
+    if (nameField.getText().isEmpty() || numberField.getText().isEmpty() || monthField.getText().isEmpty()
+        || yrField.getText().isEmpty() || securityCodeField.getText().isEmpty() || postalCodeField.getText().isEmpty() || pinField.getText().isEmpty()) {
+      errorLabel.setText("Please fill in all the fields");
+      return;
+    }
+
+    if (!numberField.getText().matches("[0-9]+")) {
+      errorLabel.setText("Invalid Card Number");
+      return;
+    }
+
+    if (!monthField.getText().matches("[1-12]+")) {
+      errorLabel.setText("Invalid Month");
+      return;
+    }
+
+    if (!yrField.getText().matches("[0-9]+")) {
+      errorLabel.setText("Invalid Year");
+      return;
+    }
+
+    if (!securityCodeField.getText().matches("[0-9]+")) {
+      errorLabel.setText("Invalid Security Code");
+      return;
+    }
+
+    if (!postalCodeField.getText().matches("[0-9]+")) {
+      errorLabel.setText("Invalid Postal Code");
+      return;
+    }
+
+    if (!amountField.getText().matches("[0-9]+(\\.\\d+)?") || Double.parseDouble(amountField.getText()) <= 0.0
+        || Double.parseDouble(amountField.getText()) > 1000000.0) {
+      errorLabel.setText("Invalid Amount");
+      return;
+    }
+
+    UserService userService = new UserService();
+    int currentUserPin = userService.getCurrentUser().getPin();
+    if (!pinField.getText().equals(String.valueOf(currentUserPin))) {
+      errorLabel.setText("Invalid PIN");
+      return;
+    }
+
+    TransactionService transactionService = new TransactionService();
+    TransactionInfo destination = new TransactionInfo("Card","Debit/Credit/Prepaid");
+    TransactionInfo reference = new TransactionInfo("Card Number", numberField.getText());
+    double amount = Double.parseDouble(amountField.getText());
+
+    transactionService.addTransaction(userService.getCurrentUser(), TransactionType.ADD_MONEY_CARD,
+        destination, amount, 0.0, reference);
+
+    userService.addBalance(userService.getCurrentUser(), Double.parseDouble(amountField.getText()));
+
     SceneController.setScene("AMFromCardSuccess.fxml", "Add Money from Card");
   }
 
